569b86caa70fd891edf45b3cdeb769ee4164836e
romanprem0123 <romanprem0123@gmail.com>
2023-08-03 15:15:32 +0530

    new

---
+++ js/main.js
@@ -0,0 +1,40 @@
+	
+	// 1.print N numbers of resverse order
+	
+	// let a = 10;  //parseInt(prompt("Enter numbers"));
+	
+	// for(i=a; i>=1; i--){
+			
+			// console.log(i);
+		// }
+
+
+	//2. print N odd numbers
+	
+	// let a = 100; //parseInt(prompt("Enter numbers"));
+	
+	// for(i=1; i<=a; i++){
+		// if(i%2!=0){
+			// console.log(i);
+		// }
+	// }
+	//3. Calculate sum of a number
+
+	// let a = parseInt(prompt("Enter numbers"));
+	// let b = parseInt(prompt("Enter numbers"));
+	// c = a+b;
+	
+	// console.log("total: "+c);
+	
+	//4. Print Table of given number
+	
+	// let a = parseInt(prompt("Enter numbers"));
+
+	// for(i=1; i<=10; i++){
+		
+		// let multi = 0; 
+		
+		// multi= multi+i
+		
+		// console.log(i+" x "+a+ " = "+multi*a);
+	// }
